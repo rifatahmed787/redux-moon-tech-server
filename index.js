2ba4e59dfac476668ed8d22d380451c2c6607d1e
Rifat <mdrifatahmed787@gmail.com>
2023-03-07 03:17:42 +0600

    products getting

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("hello moontech");
+});
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bathfkv.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+async function run() {
+  try {
+    const productsCollection = client.db("moonTech").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+app.listen(port, () => {
+  console.log(`port is running on ${port}`);
+});
